f7ae90cde59fe4f1e12c91971eade2aafe16b54a
Simon Josi <simon.josi@atizo.com>
2010-11-12 19:45:58 +0100

    add new copyright notice; switch to two space indentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,5 @@
-# manifests/init.pp - manage gcc stuff
-# Copyright (C) 2008 admin@immerda.ch
-# GPLv3
-
 class gcc {
-    package{ [ 'gcc', 'gcc-c++' ]:
-        ensure => present,
-    }
+  package{[ 'gcc', 'gcc-c++' ]:
+    ensure => present,
+  }
 }
-
--- manifests/swig.pp
+++ manifests/swig.pp
@@ -1,4 +1,3 @@
-# i dunno if this is the right place, but can't think of any other currently
 class gcc::swig {
   package{'swig':
     ensure => installed,
